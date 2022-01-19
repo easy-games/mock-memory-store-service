cce5374f3e7dece5a28ac6a910fd23143ae27a7f
Jonathan <void.knight09@gmail.com>
2022-01-19 13:50:22 +1300

    Queue + SortedMap fetch should grab some queue/map

--- lib/MockMemoryStoreService/init.lua
+++ lib/MockMemoryStoreService/init.lua
@@ -1,15 +1,36 @@
 local MockMemoryStoreSortedMap = require(script.MockMemoryStoreSortedMap)
 local MockMemoryStoreQueue = require(script.MockMemoryStoreQueue)
 
-local MockMemoryStoreService = {}
+local MockMemoryStoreService = {
+    queues = {},
+    sortedMaps = {}
+}
 MockMemoryStoreService.__index = MockMemoryStoreService
 
+--[[
+    Will retrieve a MockMemoryStoreSortedMap object under the specified name
+]]
 function MockMemoryStoreService:GetSortedMap(name: string)
-    return MockMemoryStoreSortedMap.new(name)
+    local sortedMap = self.sortedMaps[name];
+    if sortedMap == nil then
+        sortedMap = MockMemoryStoreSortedMap.new(name)
+        self.sortedMaps[name] = sortedMap
+    end
+
+    return sortedMap
 end
 
+--[[
+    Will retrieve a MockMemoryStoreQueue object under the specified name
+]]
 function MockMemoryStoreService:GetQueue(name: string, timeout: number)
-    return MockMemoryStoreQueue.new(name, timeout)
+    local queue = self.queues[name]
+    if queue == nil then
+        queue = MockMemoryStoreQueue.new(name, timeout)
+        self.queues[name] = queue
+    end
+
+    return queue
 end
 
 return MockMemoryStoreService
