4737c717a316dc71c8c256bb28b982ab4e5a33ca
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-09-13 19:57:36 -0400

    Added strict typechecking and explicit typing to MockMemoryStoreQuota

--- lib/MockMemoryStoreService/MockMemoryStoreQuota.lua
+++ lib/MockMemoryStoreService/MockMemoryStoreQuota.lua
@@ -1,43 +1,62 @@
+--!strict
 
-local MockMemoryStoreQuota = {
-    quota = 1000,
-    lastQuotaCheckTime = 0
+export type MockMemoryStoreQuota = {
+	__index: MockMemoryStoreQuota,
+	_Quota: number,
+	_LastQuotaCheckTime: number,
+	GetRequestBaseQuota: (self: MockMemoryStoreQuota) -> number,
+	GetRequestPlayerQuota: (self: MockMemoryStoreQuota, numPlayers: number?) -> number,
+	Get: (self: MockMemoryStoreQuota) -> number,
+	ProcessWriteRequest: (self: MockMemoryStoreQuota) -> (),
+	ProcessReadRequest: (self: MockMemoryStoreQuota) -> (),
+	UpdateQuota: (self: MockMemoryStoreQuota) -> (),
+	new: () -> MockMemoryStoreQuota,
 }
 
-function MockMemoryStoreQuota:GetRequestBaseQuota()
+local MockMemoryStoreQuota = {} :: MockMemoryStoreQuota
+MockMemoryStoreQuota.__index = MockMemoryStoreQuota
+
+function MockMemoryStoreQuota:GetRequestBaseQuota(): number
     return 1000
 end
 
-function MockMemoryStoreQuota:GetRequestPlayerQuota(numPlayers): number
+function MockMemoryStoreQuota:GetRequestPlayerQuota(numPlayers: number?): number
     return 100 * (if numPlayers ~= nil then numPlayers else #game:GetService("Players"):GetPlayers())
 end
 
-function MockMemoryStoreQuota:Get()
-    return self.quota
+function MockMemoryStoreQuota:Get(): number
+    return self._Quota
 end
 
-function MockMemoryStoreQuota:ProcessWriteRequest()
-    if self.quota <= 0 then
+function MockMemoryStoreQuota:ProcessWriteRequest(): ()
+    if self._Quota <= 0 then
         error("Exceeded MemoryStore quota (on write)", 2)
     end
 
-    self.quota -= 1
+    self._Quota -= 1
 end
 
-function MockMemoryStoreQuota:ProcessReadRequest()
-    if self.quota <= 0 then
+function MockMemoryStoreQuota:ProcessReadRequest(): ()
+    if self._Quota <= 0 then
         error("Exceeded MemoryStore quota (on read)", 2)
     end
 
-    self.quota -= 1
+    self._Quota -= 1
 end
 
-function MockMemoryStoreQuota:UpdateQuota()
+function MockMemoryStoreQuota:UpdateQuota(): ()
     -- Every 60 seconds, we're resetting teh quota.
-    if self.lastQuotaCheckTime < tick() + 60 then
-        self.quota = self:GetRequestBaseQuota() + self:GetRequestPlayerQuota()
-        self.lastQuotaCheckTime = tick()
+    if self._LastQuotaCheckTime < tick() + 60 then
+        self._Quota = self:GetRequestBaseQuota() + self:GetRequestPlayerQuota()
+        self._LastQuotaCheckTime = tick()
     end
 end
 
-return MockMemoryStoreQuota
+function MockMemoryStoreQuota.new(): MockMemoryStoreQuota
+	local self: MockMemoryStoreQuota = setmetatable({}, MockMemoryStoreQuota) :: any
+	self._Quota = 1000
+	self._LastQuotaCheckTime = 0
+	return self
+end
+
+return MockMemoryStoreQuota.new()
